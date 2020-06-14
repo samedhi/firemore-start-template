30cede354f42e1320997954c503be1fbc8944cbd
Stephen Cagle <stephen@read-line.com>
2020-06-14 15:26:58 -0700

    Add and get working material-ui and reagent

--- src/zip_viewer/core.cljs
+++ src/zip_viewer/core.cljs
@@ -1,3 +1,21 @@
-(ns zip-viewer.core)
+(ns ^:figwheel-hooks zip-viewer.core
+  (:require
+   [goog.dom :as goog.dom]
+   [reagent.core :as reagent]
+   [reagent.dom :as reagent.dom]
+   [zip-viewer.mui :as mui]))
 
-(println "Hello World")
+(defn main-panel []
+  (fn []
+    [mui/container
+     "Welcome to the zip-viewer app!"]))
+
+(defn mount-root []
+  (when-let [el (goog.dom/getElement "app")]
+    (println :load-this-bad-boy!)
+    (reagent.dom/render [main-panel] el)))
+
+(defn ^:dev/after-load init []
+  (mount-root))
+
+(init)
